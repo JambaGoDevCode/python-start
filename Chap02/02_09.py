84d1ba240c2532f3256a2efd2ee66a41f5d01afd
Joao Jamba <jambapedrojoao@gmail.com>
2023-04-24 12:47:16 +0100

    feacture: python start

---
+++ Chap02/02_09.py
@@ -0,0 +1,11 @@
+# syntax error
+print("Hello world")
+
+# runtime error
+
+# semantic error
+name = "Alice"
+print(name)
+
+
+10 * (2/0)
---
+++ Chap05/test.py
@@ -0,0 +1,17 @@
+# def compare():
+#  print("is greater than")
+
+
+#def mult_inv(num):
+#  return 1/num
+#esult=mult_inv(3)
+#print(result)
+
+
+def isEven(num):
+  return num % 2 == 0
+
+if isEven(3):
+  print("3 is even")
+else:
+  print("3 is not even")
