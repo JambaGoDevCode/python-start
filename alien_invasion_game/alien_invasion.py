711182bcf02c8a6575d846009abecb7f690bfeb1
João Jamba <jambapedrojoao@gmail.com>
2024-12-26 12:05:49 +0100

    Criando uma janela do pygame e respondendo às entradas do usuário - Definingo a cor de fundo

--- alien_invasion_game/alien_invasion.py
+++ alien_invasion_game/alien_invasion.py
@@ -2,12 +2,20 @@ import pygame
 import sys
 
 def run_game():
-    pygame.init()
+    # Inicializando o jogo e cria um objeto para a tela
+    pygame.init() 
     screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Alien Invasion")
-    while True:
-        for event in pygame.event.get(): 
+    bg_color = (230, 230, 230)
+        # Inicia olaço principal do jogo 
+    while True: 
+        # Observa enventos de teclado e de mouse
+        for event in pygame.event.get(): #
             if event.type == pygame.QUIT: 
                 sys.exist()
-                pygame.display.flip()
+                # Deixa a tela mais recente visível 
+        # Redesenha a tela a cada passagem pelo laço
+        screen.fill(bg_color)
+        pygame.display.flip()
+        
 run_game()
