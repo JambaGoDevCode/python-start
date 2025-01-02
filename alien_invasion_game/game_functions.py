d37d3976bbdc694689738a7a1228a1db5bbf0bfd
João Jamba <jambapedrojoao@gmail.com>
2025-01-02 14:45:20 +0100

    Refatorando check_events function

--- alien_invasion_game/game_functions.py
+++ alien_invasion_game/game_functions.py
@@ -8,17 +8,24 @@ def Check_envents(rocket):
         if event.type == pygame.QUIT: 
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                rocket.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                rocket.moving_left = True
-                # rocket.rect.centerx += 1
+            check_keydown_events(event,rocket)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                rocket.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                rocket.moving_left = False
-
+            check_keyup_events(event, rocket)
+            
+def check_keydown_events(event, rocket):
+    """Responde a pressionamentos de tecla"""
+    if event.key == pygame.K_RIGHT:
+        rocket.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        rocket.moving_left = True
+        # rocket.rect.centerx += 1
+        
+def check_keyup_events(event, rocket):
+    """Responde a solturas de tecla"""
+    if event.key == pygame.K_RIGHT:
+        rocket.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        rocket.moving_left = False
 
 def Update_screen(ai_settings, screen, rocket):
     """Atualiza as imagens na tela e alterna para a nova tela."""
