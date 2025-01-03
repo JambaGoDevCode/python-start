71fe850c797843a6705afa85313b8626ea6a51e9
João Jamba <jambapedrojoao@gmail.com>
2025-01-03 11:48:48 +0100

    Armazenando projéteis em um grupo

--- alien_invasion_game/alien_invasion.py
+++ alien_invasion_game/alien_invasion.py
@@ -1,19 +1,23 @@
 import pygame
+from pygame.sprite import Group
+import game_functions as gf
 from settings import Settings
 from rocket import Rocket
-import game_functions as gf
 
 def run_game():
-    # Inicializando o jogo e cria um objeto para a tela
+    """Inicializando o jogo e cria um objeto para a tela"""
     pygame.init() 
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
     """Criar uma espaçonave"""
     rocket = Rocket(ai_settings,screen)
-        # Inicia olaço principal do jogo 
+    """Cria um grupo no qual serão armazenados os projéteis"""
+    bullets = Group()
+    """Inicia olaço principal do jogo"""
     while True: 
-        gf.Check_envents(rocket)
+        gf.Check_envents(ai_settings, screen,rocket, bullets)
         rocket.Update()
-        gf.Update_screen(ai_settings, screen, rocket)
+        bullets.UpdateBullet()
+        gf.Update_screen(ai_settings, screen, rocket, bullets)
 run_game()
