5243300bacd56f244c2f945d939fc9de8c8a2f5b
João Jamba <jambapedrojoao@gmail.com>
2025-02-24 14:48:46 +0100

    Concatenate Of a string

---
+++ Pytuto/2_strinngs/concatenateOfstring.py
@@ -0,0 +1,10 @@
+# Concatenate od string 
+
+message = "Hello World! "
+question = "How many people are living on the earth?"
+statement = message + " " + question
+
+print(statement)
+
+# printing a string 4 times
+print(4 * message)
