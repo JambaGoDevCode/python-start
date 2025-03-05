54f0079ed0008bcce336f043611bdac2f38c3555
João Jamba <jambapedrojoao@gmail.com>
2025-03-05 14:34:13 +0100

    Indexing Lists Python

---
+++ Pytuto/3_lists/indexing.py
@@ -0,0 +1,23 @@
+# Creating a list
+newList = ['Python', 34, 2993]
+print(newList)
+print(newList[0])
+
+print('Positive and negative indexing of the first element: \n - Positive index:', newList[0], '\n - Negative index:', newList[-3])
+print('Positive and negative indexing of the first element: \n - Positive index:', newList[2], '\n - Negative index:', newList[-2])
+print('Positive and negative indexing of the first element: \n - Positive index:', newList[1], '\n - Negative index:', newList[-1])
+
+
+'''
+What can content a list ?
+String
+Floats
+Integer
+Boolean
+Nested List
+Nested Tuple
+Other data structures
+'''
+
+contentList = ['Python', 3.45, 2998, [1,2,3,4,5,6,7,5,4,3], ('Hello', 'Python', 234,43, 34534)]
+print(contentList)
